0711e25fafc296a7c928970592ae74abf75b8c0d
Felipe Campos Pinto <felipe.campos@implantainformatica.com.br>
2022-08-30 15:26:03 -0300

    Instalando EF

---
+++ src/ImplantaDEVTraining.Data/Categorias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplantaDEVTraining.Data
+{
+    public class Categorias
+    {
+
+    }
+}
