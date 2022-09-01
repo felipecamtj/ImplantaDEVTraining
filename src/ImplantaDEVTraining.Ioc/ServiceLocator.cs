b1ba737a347f04f5fc5a33a5d07fee8f4e5f8add
Felipe Campos Pinto <felipe.campos@implantainformatica.com.br>
2022-09-01 17:16:02 -0300

    Criando projeto IOC

---
+++ src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
@@ -0,0 +1,17 @@
+﻿using ImplantaDEVTraining.Business.Concret;
+using ImplantaDEVTraining.Business.Contract;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace ImplantaDEVTraining.Ioc
+{
+    public static class ServiceLocator
+    {
+        public static void Bind(IKernel kernel)
+        {
+            kernel.Bind<ICategoriasBusiness>().To<CategoriasBusiness>().InRequestScope();
+            kernel.Bind<IEnderecosBusiness>().To<EnderecosBusiness>().InRequestScope();
+            kernel.Bind<IProfissionaisBusiness>().To<ProfissionaisBusiness>().InRequestScope();
+        }
+    }
+}
