05fbe8ffb73d2e8f5145a592f4d8a7c7dcc41104
Sandoval Cícero de Sousa <Sandoval.Sousa@implantainformatica.com.br>
2022-09-02 10:45:42 -0300

    Criando a branch

--- src/ImplantaDEVTraining.MvcApplication/Controllers/HomeController.cs
+++ src/ImplantaDEVTraining.MvcApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ImplantaDEVTraining.MvcApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page de sandoval.";
 
             return View();
         }
