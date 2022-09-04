a047f1d25f2efeed06d6fc46223f4390cc24586a
alexandre lasse <lexandre_rsl@live.com>
2022-09-04 19:41:27 -0300

    Mapeamento de classe automática (ninject);

--- src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
+++ src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
@@ -1,6 +1,5 @@
-﻿using ImplantaDEVTraining.Business.Concret;
-using ImplantaDEVTraining.Business.Contract;
-using Ninject;
+﻿using Ninject;
+using Ninject.Extensions.Conventions;
 using Ninject.Web.Common;
 
 namespace ImplantaDEVTraining.Ioc
@@ -9,9 +8,13 @@ namespace ImplantaDEVTraining.Ioc
     {
         public static void Bind(IKernel kernel)
         {
-            kernel.Bind<ICategoriasBusiness>().To<CategoriasBusiness>().InRequestScope();
-            kernel.Bind<IEnderecosBusiness>().To<EnderecosBusiness>().InRequestScope();
-            kernel.Bind<IProfissionaisBusiness>().To<ProfissionaisBusiness>().InRequestScope();
+            //https://stackoverflow.com/questions/9824863/convention-based-dependency-injection-with-ninject-3-0-0 
+
+            kernel.Bind(x => x.FromAssembliesMatching("..\\..\\ImplantaDEVTraining.Ioc\\bin\\Debug\\ImplantaDEVTraining.Business.Concret.dll")
+                  .SelectAllClasses()
+                  .BindAllInterfaces()
+                  .Configure(o => o.InRequestScope()));
+
         }
     }
 }
