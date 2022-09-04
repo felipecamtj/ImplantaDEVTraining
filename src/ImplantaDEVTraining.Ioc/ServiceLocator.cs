f384c4890146548f1fc37438abcdf8110309e4b7
Sandoval Cícero de Sousa <Sandoval.Sousa@implantainformatica.com.br>
2022-09-04 13:28:19 -0300

    Implementado o mapeamento das classes de forma automática (ninject)

--- src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
+++ src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
@@ -1,6 +1,7 @@
 ﻿using ImplantaDEVTraining.Business.Concret;
 using ImplantaDEVTraining.Business.Contract;
 using Ninject;
+using Ninject.Extensions.Conventions;
 using Ninject.Web.Common;
 
 namespace ImplantaDEVTraining.Ioc
@@ -9,9 +10,14 @@ namespace ImplantaDEVTraining.Ioc
     {
         public static void Bind(IKernel kernel)
         {
-            kernel.Bind<ICategoriasBusiness>().To<CategoriasBusiness>().InRequestScope();
-            kernel.Bind<IEnderecosBusiness>().To<EnderecosBusiness>().InRequestScope();
-            kernel.Bind<IProfissionaisBusiness>().To<ProfissionaisBusiness>().InRequestScope();
+            kernel.Bind(x => x.FromAssembliesMatching("~/bin/DebugImplantaDEVTraining.Business.Concret.dll")
+                  .SelectAllClasses()
+                  .BindAllInterfaces()
+                  .Configure(o => o.InRequestScope()));
+
+            //kernel.Bind<ICategoriasBusiness>().To<CategoriasBusiness>().InRequestScope();
+            //kernel.Bind<IEnderecosBusiness>().To<EnderecosBusiness>().InRequestScope();
+            //kernel.Bind<IProfissionaisBusiness>().To<ProfissionaisBusiness>().InRequestScope();
         }
     }
 }
