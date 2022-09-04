43b9a6e840b32c92a9dd2df96e5e57b8e444ca65
alexandre lasse <lexandre_rsl@live.com>
2022-09-04 19:42:57 -0300

    Criação da classe Filter; Criação da View Model;

---
+++ src/ImplantaDEVTraining.Entity/FilterEntity/ProfissionaisCategoriasFilterEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ImplantaDEVTraining.Entity.FilterEntity
+{
+    public class ProfissionaisCategoriasFilterEntity
+    {
+        public string NomeCategoria {get; set;}
+        public bool? CategoriaAtivo {get; set;} = true;
+        public bool? ProfissionalAtivo { get; set;} = true;
+
+    }
+}
---
+++ src/ImplantaDEVTraining.Entity/ProfissionaisCategoriasEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImplantaDEVTraining.Entity
+{
+    public class ProfissionaisCategoriasEntity
+    {
+        public string Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+
+    }
+    public class CategoriasProfissionaisEntity
+    {
+        public CategoriasProfissionaisEntity()
+        {
+            Profissionais = new List<ProfissionaisCategoriasEntity>();
+
+        }
+        public string NomeCategoria { get; set; }
+        public ICollection<ProfissionaisCategoriasEntity> Profissionais { get; set; }
+
+
+    }
+}
