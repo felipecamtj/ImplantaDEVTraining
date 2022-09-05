1ffd4f105c835c418841634567e5090e46d8fc5f
Felipe Campos Pinto <felipe.campos@implantainformatica.com.br>
2022-09-05 16:24:11 -0300

    Criando controller base

---
+++ src/ImplantaDEVTraining.MvcApplication/Controllers/BaseController.cs
@@ -0,0 +1,80 @@
+﻿using ImplantaDEVTraining.Common;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ImplantaDEVTraining.MvcApplication.Controllers
+{
+    public abstract class BaseController<TBusiness, TEntity, TFilter>: Controller
+        where TBusiness: IBusiness<TEntity, TFilter>
+        where TEntity: BaseEntity, new()
+        where TFilter: BaseFilterEntity, new()
+    {
+        protected readonly TBusiness _business;
+
+        protected BaseController(TBusiness business)
+        {
+            _business = business;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Editar(Guid? id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult BuscarFiltro()
+        {
+            var filtro = new TFilter
+            {
+                Ids = new List<Guid>()
+            };
+
+            return Json( new { data = filtro }, JsonRequestBehavior.AllowGet);            
+        }
+
+        [HttpPost]
+        public JsonResult Salvar(TEntity entity)
+        {
+            if (entity.Acao == EntityAction.None)
+                entity.Acao = EntityAction.Update;
+
+            var operacao = new Operacao<TEntity>(entity);
+            operacao = _business.Salvar(operacao);
+
+            return Json( new { data = operacao }, JsonRequestBehavior.DenyGet);
+        }
+
+        [HttpGet]
+        public JsonResult BuscarRegistros(TFilter filtro)
+        {
+            var registros = _business.BuscarRegistros(filtro);
+            return Json( new { data = registros }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected virtual TEntity NewEntity()
+        {
+            return new TEntity
+            {
+                Acao = EntityAction.New,
+                Id = Guid.NewGuid()
+            };
+        }
+
+        [HttpGet]
+        public JsonResult BuscarRegistro(Guid? id)
+        {
+            var registro = id.HasValue
+                ? _business.BuscarRegistro(id.Value)
+                : NewEntity();
+
+            return Json(new { data = registro }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/ImplantaDEVTraining.MvcApplication/Controllers/CategoriasController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImplantaDEVTraining.MvcApplication.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        [HttpPost]
+        public string Mensagem()
+        {
+            return "Esta é uma mensagem de teste";
+        }
+
+        public DateTime DataAtual()
+        {
+            return DateTime.Now;
+        }
+
+        public int Idade()
+        {
+            return 30;
+        }
+
+        // GET: Categorias
+        public ActionResult Index(string nome, int idade)
+        {
+            ViewBag.Nome = nome;
+            ViewData["Idade"] = idade;
+            return View();
+        }
+    }
+}
