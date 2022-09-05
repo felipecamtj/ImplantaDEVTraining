51f6266d50fafbea0efd551e2d1e6d8a592a0833
Rossano Alves Lima Júnior <rossano.junior1@gmail.com>
2022-09-05 10:12:39 -0300

    Mapeamento com Get

--- src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
+++ src/ImplantaDEVTraining.Ioc/ServiceLocator.cs
@@ -1,6 +1,7 @@
 ﻿using ImplantaDEVTraining.Business.Concret;
 using ImplantaDEVTraining.Business.Contract;
 using Ninject;
+using Ninject.Modules;
 using Ninject.Web.Common;
 
 namespace ImplantaDEVTraining.Ioc
@@ -9,9 +10,9 @@ namespace ImplantaDEVTraining.Ioc
     {
         public static void Bind(IKernel kernel)
         {
-            kernel.Bind<ICategoriasBusiness>().To<CategoriasBusiness>().InRequestScope();
-            kernel.Bind<IEnderecosBusiness>().To<EnderecosBusiness>().InRequestScope();
-            kernel.Bind<IProfissionaisBusiness>().To<ProfissionaisBusiness>().InRequestScope();
+            CategoriasBusiness categorias = kernel.Get<CategoriasBusiness>();
+            EnderecosBusiness enderecos = kernel.Get<EnderecosBusiness>();
+            ProfissionaisBusiness profissioanl = kernel.Get<ProfissionaisBusiness>();
         }
     }
 }
